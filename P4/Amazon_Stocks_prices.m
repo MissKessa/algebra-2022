016d27ae74647bbf337ee67e0d1fadbc27597849
Paula <132788713+MissKessa@users.noreply.github.com>
2023-10-07 21:30:26 +0200

    Add files via upload

---
+++ P4/Amazon_Stocks_prices.m
@@ -0,0 +1,6 @@
+% Prises of Amazon
+% https://www.macrotrends.net/stocks/charts/AMZN/amazon/stock-splits
+%% Data:
+years=[2010:2020];
+% Stock prices before the split:
+stocks=[136.25 181.36 175.88 256.07 398.79 312.57 656.28 757.92 1172 1465.2 1875 3206 2991.47];
